873b176468a3bccd3770e9324143524f35427978
AlexeyDvornikov <dvornikovdev@gmail.com>
2018-04-06 23:42:11 +0300

    добавлена поддержка spring

--- src/com/cb/config/Main.java
+++ src/com/cb/config/Main.java
@@ -5,19 +5,31 @@ import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Lazy;
 
-public class Main extends Application {
+@Lazy
+@SpringBootApplication
+public class Main extends AbstractJavaFxApplicationSupport {
+
+    @Value("${ui.title:JavaFX приложение}")//
+    private String windowTitle;
+
+//    @Autowired
+//    private ControllersConfig.View view;
 
     @Override
-    public void start(Stage primaryStage) throws Exception{
+    public void start(Stage stage) throws Exception {
         Parent root = FXMLLoader.load(getClass().getResource("../ui/sample.fxml"));
-        primaryStage.setTitle("Hello World");
-        primaryStage.setScene(new Scene(root, 300, 275));
-        primaryStage.show();
+        stage.setTitle("Hello World");
+        stage.setScene(new Scene(root, 300, 275));
+        stage.show();
     }
-
 
     public static void main(String[] args) {
-        launch(args);
+        launchApp(Main.class, args);
     }
+
 }
